7b85c9575922d8695da722c415b3fca0c33a27c2
Janus Dalsgaard Schnabl <s235265@student.dtu.dk>
2024-01-03 10:58:05 +0100

    Uploaded project files

---
+++ snake/src/main/java/com/example/Point.java
@@ -0,0 +1,18 @@
+package com.example;
+
+// Point class - Defines the coordinates of the snake - is used for both the snake and replay
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void getPos() {
+
+    }
+
+}
