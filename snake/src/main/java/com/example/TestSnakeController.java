3e7d04864b1210b25b728f644bf6f3f4d9d58e64
Siam <s235242@student.dtu.dk>
2024-01-03 13:19:37 +0100

    canvastest

--- snake/src/main/java/com/example/TestSnakeController.java
+++ snake/src/main/java/com/example/TestSnakeController.java
@@ -2,8 +2,25 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.aplication.Aplication;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class TestSnakeController {
+    private static final int WIDTH = 640;
+    private static final int HEIGHT = WIDTH;
+    private static final int ROWS = 20;
+    private static final int COLUMMS = ROWS;
+    private static final int SQUARE_SIZE = WIDTH / ROWS;
+
+
+
+
+
+
+
 
     @FXML
     private void switchToSecondary() throws IOException {
