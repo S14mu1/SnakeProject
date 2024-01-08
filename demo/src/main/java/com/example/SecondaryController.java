6be336c6903de457f56bb196892e1101f4354802
Christian Paus Damsgaard <ChristianPausD@gmail.com>
2024-01-08 12:30:08 +0100

    Testing

--- demo/src/main/java/com/example/SecondaryController.java
+++ demo/src/main/java/com/example/SecondaryController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class SecondaryController {
-
+   //test test test
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
